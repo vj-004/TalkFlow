d1a8d68c100aa6f34f871e334f495b7695bb2ba8
vj-004 <vj20190204@gmail.com>
2024-10-29 22:24:20 +0530

    Ready for Deployment

--- server/index.js
+++ server/index.js
@@ -7,13 +7,16 @@ import authRoutes from "./routes/AuthRoutes.js";
 import contactsRoutes from "./routes/ContactRoutes.js";
 import setupSocket from "./socket.js";
 import messagesRoutes from "./routes/MessagesRoutes.js";
-
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
+
+const _dirname = path.resolve();
+
 
 app.use(cors({
     origin: [process.env.ORIGIN],
@@ -30,7 +33,10 @@ app.use('/api/auth', authRoutes);
 app.use("/api/contacts",contactsRoutes)
 app.use("/api/messages", messagesRoutes);
 
-
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (req,res) => {
+    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+});
 
 
 const server = app.listen(port,() => {
